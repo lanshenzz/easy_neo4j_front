d1503b373b620b2bafdd201f549f18d6391d76d0
lanshenzz <495102181@qq.com>
2020-04-25 23:18:58 +0800

    添加

--- neo4j_front/src/components/HelloWorld.vue
+++ neo4j_front/src/components/HelloWorld.vue
@@ -2,14 +2,22 @@
   <div class="hello">
     <el-row>
       <el-col :span="8">
-        <div><el-button>查询所有人</el-button></div></el-col>
+        <div>
+          <el-button @click="init">查询所有人</el-button>
+          <el-input v-model="input" placeholder="依据姓名模糊查询"></el-input>
+        </div></el-col>
       <el-col :span="16"><div>
         <el-table
           :data="user"
           style="width: 100%">
           <el-table-column
-            prop="name"
-            label="日期"
+          prop="name"
+          label="姓名"
+          width="180">
+        </el-table-column>
+          <el-table-column
+            prop="sex"
+            label="性别"
             width="180">
           </el-table-column>
           <el-table-column
@@ -37,11 +45,12 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      user:[]
+      user:[],
+      input:''
     }
   },
   created(){
-    this.init()
+  //  this.init()
   },
   methods:{
    init(){
@@ -49,25 +58,13 @@ export default {
        this.user=res.data
        console.log("this.user="+JSON.stringify(this.user));
      })
-   }
+   },
+
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
--- neo4j_front/src/main.js
+++ neo4j_front/src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App'
 import axios from "axios";
 import ElementUI from 'element-ui';
-Vue.use(ElementUI);
+
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
 axios.defaults.baseURL = '/api';
 Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
