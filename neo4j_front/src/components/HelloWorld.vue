37944df2b03c715f810abb8a2d9b69b4a4228b17
lanshenzz <495102181@qq.com>
2020-04-25 23:05:41 +0800

    添加

--- neo4j_front/src/components/HelloWorld.vue
+++ neo4j_front/src/components/HelloWorld.vue
@@ -1,16 +1,55 @@
 <template>
   <div class="hello">
+    <el-row>
+      <el-col :span="8">
+        <div><el-button>查询所有人</el-button></div></el-col>
+      <el-col :span="16"><div>
+        <el-table
+          :data="user"
+          style="width: 100%">
+          <el-table-column
+            prop="name"
+            label="日期"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="age"
+            label="年龄"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="hobby"
+            label="爱好"
+            width="180">
+          </el-table-column>
+        </el-table>
+      </div>
+
+    </el-col>
+    </el-row>
 
   </div>
 </template>
 
-<script>
+<script >
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-
+      user:[]
     }
+  },
+  created(){
+    this.init()
+  },
+  methods:{
+   init(){
+     this.$axios.get('/user/getAll').then(res=>{
+       this.user=res.data
+       console.log("this.user="+JSON.stringify(this.user));
+     })
+   }
   }
 }
 </script>
--- neo4j_front/src/main.js
+++ neo4j_front/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App'
-
+import axios from "axios";
+import ElementUI from 'element-ui';
+Vue.use(ElementUI);
+axios.defaults.baseURL = '/api';
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
